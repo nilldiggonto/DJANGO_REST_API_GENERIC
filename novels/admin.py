77508b15c44528a557494f2e288de25f9627e874
nilldiggonto <nilldiggonto@gmail.com>
2020-11-19 23:49:53 +0600

    Model Created

--- novels/admin.py
+++ novels/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Novel,Review
 # Register your models here.
+admin.site.register(Novel)
+admin.site.register(Review)
--- novels/models.py
+++ novels/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator,MaxValueValidator
 # Create your models here.
+class Novel(models.Model):
+    title           = models.CharField(max_length=120)
+    author          = models.CharField(max_length=120)
+    description     = models.TextField()
+    publish_date    = models.DateField()
+
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    created_at      = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
+    review_author   = models.CharField(max_length=10, null=True,blank=True)
+    review          = models.TextField(blank=True,null=True)
+    rating          = models.PositiveIntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    novel           = models.ForeignKey(Novel,on_delete= models.CASCADE,related_name='review')
+
+    def __str__(self):
+        return str(self.rating)
